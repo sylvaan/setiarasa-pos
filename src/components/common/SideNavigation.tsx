3ba164bf8b7853bb07dbaf71fa9866dfb90a26c4
Ahmad Ridhwan <arnaufal96@gmail.com>
2026-04-12 12:18:27 +0700

    chore: bump version to v1.1.8

--- src/components/common/SideNavigation.tsx
+++ src/components/common/SideNavigation.tsx
@@ -282,7 +282,7 @@ export const SideNavigation = ({
                 </div>
               )}
               <p className="!text-center !text-[8px] !text-slate-300 !mt-4 !uppercase !tracking-[0.2em] !font-medium">
-                Martabak SetiaRasa &bull; v1.1.5
+                Martabak SetiaRasa &bull; v1.1.8
               </p>
             </div>
           </motion.div>
